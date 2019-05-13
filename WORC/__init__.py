6841fedd5d616dc1f8758f15544f3b36e38ee638
Thomas <t.phil@erasmusmc.nl>
2019-05-13 10:22:42 +0200

    Started working on quickradiomics facade

--- WORC/__init__.py
+++ WORC/__init__.py
@@ -1,2 +1,5 @@
 from WORC.WORC import WORC
 from WORC import classification, featureprocessing, processing
+from .facade import I
+
+i = I()
---
+++ WORC/facade/jipenjannekefacade/__init__.py
@@ -0,0 +1,1 @@
+from .jipenjannekefacade import JipEnJannekeFacade
