33efa917230cea753f97b9b541370fcc141c1672
Thomas <t.phil@erasmusmc.nl>
2019-05-27 17:15:42 +0200

    Bugfix

--- WORC/facade/intermediatefacade/exceptions.py
+++ WORC/facade/intermediatefacade/exceptions.py
@@ -2,10 +2,12 @@ class PathNotFoundException(Exception):
     def __init__(self, path):
         super(PathNotFoundException, self).__init__(f'Path not found: {path}')
 
+
 class NoImagesFoundException(Exception):
     def __init__(self, path):
-        super(PathNotFoundException, self).__init__(f'No images found in directory {path}')
+        super(NoImagesFoundException, self).__init__(f'No images found in directory {path}')
+
 
 class NoSegmentationsFoundException(Exception):
     def __init__(self, path):
-        super(PathNotFoundException, self).__init__(f'No segmentations found in directory {path}')
+        super(NoSegmentationsFoundException, self).__init__(f'No segmentations found in directory {path}')
