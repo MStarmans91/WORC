38c35ccb001a7fd7530287a891bba1b71bc41a15
MStarmans91 <m.starmans@erasmusmc.nl>
2020-02-03 10:00:45 +0100

    Solved import bug.

--- WORC/plotting/__init__.py
+++ WORC/plotting/__init__.py
@@ -1,2 +1,4 @@
-import WORC.plotting.compute_CI, WORC.plotting.plot_barchart, WORC.plotting.plot_boxplot, WORC.plotting.plot_images, WORC.plotting.plot_ranked_scores
+import WORC.plotting.compute_CI, WORC.plotting.plot_barchart
+import WORC.plotting.plot_boxplot_features, WORC.plotting.plot_images
+import WORC.plotting.plot_boxplot_performance, WORC.plotting.plot_ranked_scores
 import WORC.plotting.plot_ROC, WORC.plotting.plot_SVM, WORC.plotting.scatterplot
