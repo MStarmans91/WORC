da70caefd85b41e10bc94ded32c93efbe99e73a0
MStarmans91 <m.starmans@erasmusmc.nl>
2020-05-11 11:55:28 +0200

    Bugfixes in ComBat.

--- WORC/featureprocessing/ComBat.py
+++ WORC/featureprocessing/ComBat.py
@@ -59,13 +59,26 @@ def ComBat(features_train_in, labels_train, config, features_train_out,
     if excluded_features:
         # Determine indices of excluded features
         included_feature_indices = []
+        excluded_feature_indices = []
         for fnum, i in enumerate(feature_labels):
             if not any(e in i for e in excluded_features):
                 included_feature_indices.append(fnum)
+            else:
+                excluded_feature_indices.append(fnum)
 
         # Actually exclude the features
-        image_features_train = [np.asarray(i)[included_feature_indices].tolist() for i in image_features_train]
-        feature_labels = np.asarray(feature_labels)[included_feature_indices].tolist()
+        image_features_train_combat = [np.asarray(i)[included_feature_indices].tolist() for i in image_features_train]
+        feature_labels_combat = np.asarray(feature_labels)[included_feature_indices].tolist()
+
+        image_features_train_noncombat = [np.asarray(i)[excluded_feature_indices].tolist() for i in image_features_train]
+        feature_labels_noncombat = np.asarray(feature_labels)[excluded_feature_indices].tolist()
+
+    else:
+        image_features_train_combat = image_features_train
+        feature_labels_combat = feature_labels
+
+        image_features_train_noncombat = []
+        feature_labels_noncombat = []
 
     # Apply a scaler to the features
     if scaler:
@@ -86,21 +99,27 @@ def ComBat(features_train_in, labels_train, config, features_train_out,
         image_features_test = [i[0] for i in image_features_test]
 
         if excluded_features:
-            image_features_test = [np.asarray(i)[included_feature_indices].tolist() for i in image_features_test]
+            image_features_test_combat = [np.asarray(i)[included_feature_indices].tolist() for i in image_features_test]
+            image_features_test_noncombat = [np.asarray(i)[excluded_feature_indices].tolist() for i in image_features_test]
+        else:
+            image_features_test_combat = image_features_test
+            image_features_test_noncombat = []
 
+        # Apply a scaler to the features
         if scaler:
             image_features_test = scaler.transform(image_features_test)
 
+        # Remove features with a constant value
         if VarianceThreshold:
             image_features_test = VarSel.transform(image_features_test)
 
-        all_features = image_features_train.tolist() + image_features_test.tolist()
+        all_features = image_features_train_combat.tolist() + image_features_test_combat.tolist()
         all_labels = list()
         for i in range(label_data_train['label'].shape[0]):
             all_labels.append(label_data_train['label'][i, :, 0].tolist() + label_data_test['label'][i, :, 0].tolist())
         all_labels = np.asarray(all_labels)
     else:
-        all_features = image_features_train.tolist()
+        all_features = image_features_train_combat.tolist()
         all_labels = label_data_train['label']
 
     # Convert data to a single array
@@ -139,10 +158,18 @@ def ComBat(features_train_in, labels_train, config, features_train_out,
                     'feature_labels']
 
     feature_values_train_combat = [data_harmonized[i] for i in range(len(image_features_train))]
+    feature_labels = feature_labels_combat + feature_labels_noncombat
     for fnum, i_feat in enumerate(feature_values_train_combat):
+        # Combine ComBat and non-ComBat features
+        feature_values_temp = i_feat + image_features_train_noncombat[i]
+
+        # Sort based on feature label
+        feature_labels_temp, feature_values_temp =\
+            zip(*sorted(zip(feature_labels, feature_values_temp)))
+
         # Convert to pandas Series and save as hdf5
-        panda_data = pd.Series([parameters, i_feat,
-                                feature_labels],
+        panda_data = pd.Series([parameters, feature_values_temp,
+                                feature_labels_temp],
                                index=panda_labels,
                                name=name
                                )
@@ -152,11 +179,18 @@ def ComBat(features_train_in, labels_train, config, features_train_out,
 
     # Repeat for testing if required
     if features_test_in:
-        feature_values_test_combat = [d for d in data_harmonized[len(image_features_train):]]
+        feature_values_test_combat = [d for d in data_harmonized[len(image_features_test):]]
         for fnum, i_feat in enumerate(feature_values_test_combat):
+            # Combine ComBat and non-ComBat features
+            feature_values_temp = i_feat + image_features_test_noncombat[i]
+
+            # Sort based on feature label
+            feature_labels_temp, feature_values_temp =\
+                zip(*sorted(zip(feature_labels, feature_values_temp)))
+
             # Convert to pandas Series and save as hdf5
-            panda_data = pd.Series([parameters, i_feat,
-                                    feature_labels],
+            panda_data = pd.Series([parameters, feature_values_temp,
+                                    feature_labels_temp],
                                    index=panda_labels,
                                    name=name
                                    )
@@ -166,12 +200,11 @@ def ComBat(features_train_in, labels_train, config, features_train_out,
 
 
 def ComBatMatlab(dat, batch, command, mod=None, par=1, per_feature='true'):
-    '''
+    """
     Run the ComBat Function Matlab script.
 
     par = 0 is non-parametric.
-    '''
-
+    """
     # Mod: default argument is empty list
     if mod is None:
         mod = []
