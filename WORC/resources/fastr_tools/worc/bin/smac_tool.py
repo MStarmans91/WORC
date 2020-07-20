5b636dec658bfdd409a3536f153f420c6468e0e4
mitchelldeen <mitchell_deen@outlook.com>
2020-07-20 09:46:56 +0200

    Add potential bug fix for first run crash

--- WORC/resources/fastr_tools/worc/bin/smac_tool.py
+++ WORC/resources/fastr_tools/worc/bin/smac_tool.py
@@ -70,7 +70,7 @@ def main():
                          "output_dir": "/scratch/mdeen/SMAC_output/" + run_info['run_name'],
                          "shared_model": True,
                          "input_psmac_dirs": "/scratch/mdeen/SMAC_output/" + run_info['run_name'],
-                         "abort_on_first_run_crash": False
+                         "abort_on_first_run_crash": "true"
                          })
 
     def score_cfg(cfg):
