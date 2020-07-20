6f2eac05ca44140fcbafe171d86b2e56af7c7173
mitchelldeen <mitchell_deen@outlook.com>
2020-07-20 12:06:52 +0200

    Debugging first run crash scenario option

--- WORC/resources/fastr_tools/worc/bin/smac_tool.py
+++ WORC/resources/fastr_tools/worc/bin/smac_tool.py
@@ -71,7 +71,7 @@ def main():
                          "output_dir": "/scratch/mdeen/SMAC_output/" + run_info['run_name'],
                          "shared_model": True,
                          "input_psmac_dirs": "/scratch/mdeen/SMAC_output/" + run_info['run_name'],
-                         "abort_on_first_run_crash": "true"
+                         "abort_on_first_run_crash": "false"
                          })
 
     def score_cfg(cfg):
