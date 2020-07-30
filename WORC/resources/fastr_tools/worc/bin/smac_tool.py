9c8f0fd2a7f7ea767757a7e529ffa415ca4730a3
mitchelldeen <mitchell_deen@outlook.com>
2020-07-30 15:24:17 +0200

    Add debug print statements

--- WORC/resources/fastr_tools/worc/bin/smac_tool.py
+++ WORC/resources/fastr_tools/worc/bin/smac_tool.py
@@ -158,11 +158,15 @@ def main():
 
     # Update the result file of the optimization
     result_file = data['smac_result_file']
-    with open(result_file, 'a+') as jsonfile:
-        if os.stat(result_file).st_size == 0:
+    print('THIS FILE LOCATION IS USED: ' + result_file)
+    if os.stat(result_file).st_size == 0:
+        with open(result_file, 'a') as jsonfile:
+            print('THE FILE IS WRITTEN TO FOR THE FIRST TIME')
             smac_results = {'0': smac_stats}
             json.dump(smac_results, jsonfile, indent=4)
-        else:
+    else:
+        with open(result_file, 'r+') as jsonfile:
+            print('THE FILE IS NOW READ IN AGAIN IN A NEW ITERATION')
             smac_results = json.load(jsonfile)
             run_nr = len(smac_results)
             smac_results[str(run_nr)] = smac_stats
