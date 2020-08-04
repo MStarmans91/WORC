fc744e1d0c47e9db69c4687fb4840f41c39d12b5
mitchelldeen <mitchell_deen@outlook.com>
2020-08-04 10:55:01 +0200

    Remove old print statements for debugging

--- WORC/resources/fastr_tools/worc/bin/smac_tool.py
+++ WORC/resources/fastr_tools/worc/bin/smac_tool.py
@@ -160,10 +160,8 @@ def main():
 
     # Update the result file of the optimization
     result_file = data['smac_result_file']
-    print('THIS FILE LOCATION IS USED: ' + result_file)
     if os.path.exists(result_file):
         with open(result_file, 'r') as jsonfile:
-            print('THE FILE IS NOW READ IN AGAIN IN A NEW ITERATION')
             smac_results = json.load(jsonfile)
         run_nr = len(smac_results)
         smac_results[str(run_nr)] = smac_stats
@@ -171,7 +169,6 @@ def main():
             json.dump(smac_results, jsonfile, indent=4)
     else:
         with open(result_file, 'a') as jsonfile:
-            print('THE FILE IS WRITTEN TO FOR THE FIRST TIME')
             smac_results = {'0': smac_stats}
             json.dump(smac_results, jsonfile, indent=4)
 
