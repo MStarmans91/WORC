23cc6a9a0addfd17cbec0086129d8b62c97f5ed5
mitchelldeen <mitchell_deen@outlook.com>
2020-08-31 13:03:41 +0200

    Fix bug in filename string

--- WORC/classification/SearchCV.py
+++ WORC/classification/SearchCV.py
@@ -1533,7 +1533,7 @@ class BaseSearchCVfastr(BaseSearchCV):
 
         end_time = time.time()
         runtime = end_time - start_time
-        with open(os.path.join('scratch/mdeen/runtime/RS-runtime/', name, '/runtime.txt'), 'a') as runtimefile:
+        with open(os.path.join('/scratch/mdeen/runtime/RS-runtime/', name, '/runtime.txt'), 'a') as runtimefile:
             runtimefile.write(str(runtime) + 'seconds' + '\n')
 
         # Check whether all jobs have finished
@@ -2835,7 +2835,7 @@ class BaseSearchCVSMAC(BaseSearchCV):
 
         end_time = time.time()
         runtime = end_time - start_time
-        with open(os.path.join('scratch/mdeen/runtime/SMAC-runtime/', name, '/runtime.txt'), 'a') as runtimefile:
+        with open(os.path.join('/scratch/mdeen/runtime/SMAC-runtime/', name, '/runtime.txt'), 'a') as runtimefile:
             runtimefile.write(str(runtime) + 'seconds' + '\n')
 
         # Check whether all jobs have finished
