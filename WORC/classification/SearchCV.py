de46320689a9f09256aee45f764346bd77135523
mitchelldeen <mitchell_deen@outlook.com>
2020-10-09 12:28:03 +0200

    Test

--- WORC/classification/SearchCV.py
+++ WORC/classification/SearchCV.py
@@ -1043,7 +1043,7 @@ class BaseSearchCV(six.with_metaclass(ABCMeta, BaseEstimator,
                     X_train_values = [x[0] for x in X_train] # Throw away labels
                     X_train_values_valid = [X_train_values[i] for i in valid]
                     Y_valid_score_temp = base_estimator.predict_proba(X_train_values_valid)
-                    Y_another_one = self.predict(X_train_values_valid)
+                    Y_another_one = base_estimator.best_estimator_.predict_proba(X_train_values_valid)
                     Y_valid_score_original = self.predict_proba(X_train_values_valid)
 
                     with open('/scratch/mdeen/testfiles/predict_proba_comparison.txt', 'a') as fih:
