46215dec33c2b072c4cea0f1cfb7b376adbbb55a
mitchelldeen <mitchell_deen@outlook.com>
2020-10-19 15:20:02 +0200

    Print fit and score result

--- WORC/classification/SearchCV.py
+++ WORC/classification/SearchCV.py
@@ -1054,8 +1054,8 @@ class BaseSearchCV(six.with_metaclass(ABCMeta, BaseEstimator,
                     # Refit a SearchCV object with the provided parameters
                     #base_estimator.refit_and_score(training_set, training_labels,
                     #                               p_all, all_indices, all_indices)
-                    #ret = fit_and_score(X_train, Y_train, scoring, train, valid, p_all)
-                    #print('ret score: ' + str(ret[0][1]) + '\n')
+                    ret = fit_and_score(X_train, Y_train, scoring, train, valid, p_all)
+                    print('ret score: ' + str(ret[0][1]) + '\n')
 
                     base_estimator.refit_and_score(X_train, Y_train, p_all,
                                                    train, valid)
