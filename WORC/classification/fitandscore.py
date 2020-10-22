96e623329e68dfc726c3e59ea953c6c339b83613
mitchelldeen <mitchell_deen@outlook.com>
2020-10-22 17:40:34 +0200

    Revert

--- WORC/classification/fitandscore.py
+++ WORC/classification/fitandscore.py
@@ -876,9 +876,6 @@ def fit_and_score(X, y, scoring,
     else:
         ret[2] = runtime
 
-    with open('/scratch/mdeen/testfiles/fitprint.txt', 'a') as fitprintfile:
-        fitprintfile.write(str(ret[1]) + '\n')
-
     if return_all:
         return ret, GroupSel, VarSel, SelectModel, feature_labels[0], scaler, imputer, pca, StatisticalSel, ReliefSel, Sampler
     else:
