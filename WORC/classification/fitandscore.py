c8a3ad951d4bac1d21cb705473c28be24507a745
mitchelldeen <mitchell_deen@outlook.com>
2020-10-28 17:42:36 +0100

    Remove fitandscore file write

--- WORC/classification/fitandscore.py
+++ WORC/classification/fitandscore.py
@@ -885,8 +885,8 @@ def fit_and_score(X, y, scoring,
     else:
         ret[2] = runtime
 
-    with open('/scratch/mdeen/testfiles/fitandscore.txt', 'a') as fitandscoreprint:
-        fitandscoreprint.write('score (' + str(parameters['CNB_alpha']) + ') on split ' + str(splitnr) + ': ' + str(ret[1]) + '\n')
+    #with open('/scratch/mdeen/testfiles/fitandscore.txt', 'a') as fitandscoreprint:
+    #    fitandscoreprint.write('score (' + str(parameters['CNB_alpha']) + ') on split ' + str(splitnr) + ': ' + str(ret[1]) + '\n')
 
     if return_all:
         return ret, GroupSel, VarSel, SelectModel, feature_labels[0], scaler, imputer, pca, StatisticalSel, ReliefSel, Sampler
