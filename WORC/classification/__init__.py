773db6b450abb1a1e70e5b5626eff38928ab61ba
MStarmans91 <m.starmans@erasmusmc.nl>
2021-07-08 16:11:14 +0200

    Solve bug

--- WORC/classification/__init__.py
+++ WORC/classification/__init__.py
@@ -2,7 +2,6 @@ import WORC.classification.fitandscore
 import WORC.classification.SearchCV
 import WORC.classification.AdvancedSampler
 import WORC.classification.construct_classifier
-import WORC.classification.estimators
 import WORC.classification.metrics
 import WORC.classification.parameter_optimization
 import WORC.classification.crossval
